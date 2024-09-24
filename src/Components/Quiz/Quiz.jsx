6ba63c87aa998789cddb1a50d363372b5faaace9
henryagyare <83439356+henryagyare@users.noreply.github.com>
2024-09-24 03:43:07 +0000

    mod-2

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Quiz.css"
+import { data } from '../../assests/data';
 
 const Quiz = () => {
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(data[index])
+
+
   return (
     <div className='container'>
         <h1>React Quiz App</h1>
         <hr />
-        <h2>Which device is required for internet connections?</h2>
+        <h2>1. Which device is required for internet connections?</h2>
         <ul>
             <li>Modem</li>
             <li>Router</li>
